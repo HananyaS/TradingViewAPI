d10509bf4ac2e88c2db90aa20d801e85ad25d72e
HananyaS <shacharhananya@gmail.com>
2025-07-05 03:30:03 +0300

    update defaults

--- default_query_params.py
+++ default_query_params.py
@@ -3,8 +3,8 @@ from dataclasses import dataclass
 @dataclass
 class Defaults:
     US_EXCHANGES_ONLY = True
-    MIN_RELATIVE_VOLUME = None # 1.5
-    MIN_CHANGE = None # 0
+    MIN_RELATIVE_VOLUME = 1.5
+    MIN_CHANGE = None
     MIN_SMA20_ABOVE_PCT = 1.1
     MAX_SMA20_ABOVE_PCT = None
     MIN_ATR_PCT = 5.0
